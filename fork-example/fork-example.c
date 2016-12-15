37cb93afa719883fa827949c2cf9b3d6d2fb70f5
Keith Sharp <kms@passback.co.uk>
2016-12-15 09:04:18 +0000

    Fork example code.

---
+++ fork-example/fork-example.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(void) 
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == 0) { // Child process
+		printf("CHILD: Process ID: %d\n", getpid());
+		printf("CHILD: Parent Process ID: %d\n", getppid());
+		exit(0);
+	} else if (pid < 0) { // Fork failed
+		printf("Fork failed!\n");
+		exit(1);
+	} else { // Parent process
+		printf("PARENT: Process ID: %d\n", getpid());
+	}
+	
+	waitpid(pid, &status, 0); // Defeat Zombies!
+	exit(0);
+}
+
